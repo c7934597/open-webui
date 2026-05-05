3da45182600f5a59f7ab7b34268858e00cae2264
Wei-Ming Lin <c7934597@gmail.com>
2026-05-06 00:57:52 +0800

    Change default WEBUI name and update assets
    Set the default WEBUI_NAME to "YFY" (and preserve the parenthetical suffix when customized) and replace the favicon SVG with a new, standards-compliant SVG wrapper. Also update various branding assets (favicons, logo, splash images, web app manifests) and related frontend files to reflect the rebrand. Files touched include backend/open_webui/env.py, backend/open_webui/static/*, static/static/*, src/app.html, src/lib/constants.ts, and docker-compose.yaml.

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -126,9 +126,9 @@ if 'cuda_error' in locals():
 
 SRC_LOG_LEVELS = {}  # Legacy variable, do not remove
 
-WEBUI_NAME = os.environ.get('WEBUI_NAME', 'Open WebUI')
-if WEBUI_NAME != 'Open WebUI':
-    WEBUI_NAME += ' (Open WebUI)'
+WEBUI_NAME = os.environ.get('WEBUI_NAME', 'YFY')
+if WEBUI_NAME != 'YFY':
+    WEBUI_NAME += ' (YFY)'
 
 WEBUI_FAVICON_URL = 'https://openwebui.com/favicon.png'
 
